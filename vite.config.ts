51efe88c5cf251f994ae9640d8a705e51e1d8382
Shaun <neromaki@gmail.com>
2025-12-29 12:58:04 +0000

    Dev/Prod root URL fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: '/', // Use '/foxhole-reporter/' for GitHub Pages base, '/' for custom domain
+  base: mode === 'production' ? '/' : '/foxhole-reporter/', // Use '/foxhole-reporter/' for GitHub Pages base, '/' for custom domain
   define: {
     __APP_VERSION__: JSON.stringify(process.env.npm_package_version)
   },
